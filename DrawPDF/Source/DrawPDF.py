ea6537a99cc2ed90e4c54b460a22cae1bc30ad00
suhwan <sorktnghks@naver.com>
2019-07-03 21:49:01 +0900

    ..

--- DrawPDF/Source/DrawPDF.py
+++ DrawPDF/Source/DrawPDF.py
@@ -10,52 +10,59 @@ class DrawPDF():
 
         self._ConstructWidgets()
     def _ConstructWidgets(self):
-        #Tab 컨트롤
-        self.tabControl = ttk.Notebook(self.root)
+        # Tab 컨트롤
+        tabControl = ttk.Notebook(self.root)
 
-        self.tab1 = ttk.Frame(self.tabControl)
-        self.tabControl.add(self.tab1,text = 'tab1')
+        self._AddTab1(tabControl)
+        tabControl.pack()
 
-        self.tabControl.pack()
-
-        #------------
-        # Tab1
-        #------------
+    def _AddTab1(self,tabControl):
+        tab1 = ttk.Frame(tabControl)
+        tabControl.add(tab1,text = 'tab1')
 
         #Frame 컨트롤
-        self.mighty = ttk.LabelFrame(self.tab1,text='mighty')
-        self.mighty.grid(row=0,column=0)#padx = 200,pady = 200
+        mighty = ttk.LabelFrame(tab1,text='mighty')
+        mighty.grid(row=0,column=0)#padx = 200,pady = 200
 
         #메시지
-        tk.Label(self.mighty,text='평균입력').grid(row=0,column = 0)
-        tk.Label(self.mighty, text='표준편차입력').grid(row=0, column=1)
-        tk.Label(self.mighty, text='적용').grid(row=0, column=2)
+        tk.Label(mighty,text='평균입력').grid(row=0,column = 0)
+        tk.Label(mighty, text='표준편차입력').grid(row=0, column=1)
+        #tk.Label(self.mighty, text='적용').grid(row=0, column=2)
 
         #text Box 평균 받기
         self.tmpMean = tk.StringVar()
-        self.txtMean = ttk.Entry(self.mighty,textvariable=self.tmpMean)
+        self.txtMean = ttk.Entry(mighty,textvariable=self.tmpMean)
         self.txtMean.grid(row  = 1, column = 0)
         #text Box 표준편차 받기
         self.tmpStd = tk.StringVar()
-        self.txtStd = ttk.Entry(self.mighty,textvariable=self.tmpStd)
+        self.txtStd = ttk.Entry(mighty,textvariable=self.tmpStd)
         self.txtStd.grid(row  = 1, column = 1)
 
+        def _buttonAction():
+            mean = self.tmpMean.get()
+            std = self.tmpMean.get()
+            txt = '입력값! >> \n평균 : {m}\n표준편차 : {s}'.format(m=mean,s=std)
+            self.tmpInput.set(txt)
+
+
         #button 적용
-        self.btApply = tk.Button(self.mighty,text='적용!!')
+        self.btApply = tk.Button(mighty,text='적용!!',command = _buttonAction)
         self.btApply.grid(row=1,column=2)
+
+
         #Combo Box
         #self.tmpStd = tk.IntVar()
         #self.
 
         #Label
         self.tmpInput = tk.StringVar()
-        self.tmpInput.set('결과값은:')
-        self.inputLabel = tk.Label(self.mighty,textvariable=self.tmpInput)
+        self.tmpInput.set('입력값 >>')
+        self.inputLabel = tk.Label(mighty,textvariable=self.tmpInput)
         self.inputLabel.grid(column = 0,row = 2,columnspan = 3)
 
 
         #그림영역 확보
-        self.picture = ttk.LabelFrame(self.tab1,text='picture')
+        self.picture = ttk.LabelFrame(tab1,text='picture')
         self.picture.grid(row=1,column =0, padx = 5, pady = 5)
         #Canvas
         '''
